90b99493e4b125081a6411a5d684f3bdde0cc7dc
Felicia Baa-Adomako <feliciab840@gmail.com>
2023-05-26 22:38:33 -0400

    Files restored

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -3,9 +3,9 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-const routes = [
-  { path: "/", component: Home },
-  { path: "/medications", component: Medications },
-];
+// const routes = [
+//   { path: "/", component: Home },
+//   { path: "/medications", component: Medications },
+// ];
 
 createApp(App).mount("#app");
